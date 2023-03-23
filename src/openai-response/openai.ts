89255b019a88fd36a3829d98195cf1c4fdfb270a
lemondouble <lemondouble2@gmail.com>
2023-03-23 22:44:19 +0900

    fix : 기본 프롬프트에서 디스코드 봇이라고 적혀 있던 버그 수정..

--- src/openai-response/openai.ts
+++ src/openai-response/openai.ts
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const generatePrompt = (question: string): string => {
-  return `너는 디스코드에서 운영되고 있는 봇이야.
+  return `너는 슬랙에서 운영되고 있는 봇이야.
     너는 가능한 한 친절하게 대답해야 해.
     이 질문에 대해 답해봐.
     질문 : ${question}
